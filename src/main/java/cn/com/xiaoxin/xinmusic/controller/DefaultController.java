b3229f686e1dc39cba397b6238827ae3f6dc7c5f
小新 <743388398@qq.com>
2022-11-05 22:10:48 +0800

    调整CPU配置

--- src/main/java/cn/com/xiaoxin/xinmusic/controller/DefaultController.java
+++ src/main/java/cn/com/xiaoxin/xinmusic/controller/DefaultController.java
@@ -1,12 +1,13 @@
 package cn.com.xiaoxin.xinmusic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class DefaultController {
-    @RequestMapping
+    @GetMapping
     public String sayHello() {
         return "欢迎!!!";
     }
